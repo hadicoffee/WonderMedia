bbfaffe5be375db2fa33bcaeb67886d8a3fb7db7
hadicoffee <hadikusumaandroid@gmail.com>
2013-03-20 15:51:48 +0800

    Update cm.mk

--- WM8850/cm.mk
+++ WM8850/cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := nsfat
+PRODUCT_RELEASE_NAME := WM8850
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
